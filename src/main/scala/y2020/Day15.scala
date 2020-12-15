0f3d820d34758dc2db31cc50acdbb114342bc4f9
Eric Bowman <ebowman@boboco.ie>
2020-12-15 06:57:35 +0100

    2020/15

--- src/main/scala/y2020/Day15.scala
+++ src/main/scala/y2020/Day15.scala
@@ -1,105 +1,23 @@
 package y2020
 
-import scala.annotation.tailrec
-import scala.util.matching.Regex
+trait Day15 {
 
-trait Common {
-  val MASK: Long = (1L << 36) - 1
-  val Mask: Regex = """mask = (.*)""".r
-  val Write: Regex = """mem\[(.*?)] = (.*)""".r
-
-  def allocateMem(): collection.mutable.Map[Long, Long] = {
-    new collection.mutable.HashMap[Long, Long]() {
-      override def default(key: Long): Long = 0L
+  def solve(input: String, limit: Int): Int = {
+    val memory = new collection.mutable.HashMap[Int, (Int, Int)]() {
+      override def default(k: Int) = (-1, -1)
     }
+
+    val seeds = input.split(",").map(_.toInt).toIndexedSeq
+    for (t <- 1 to seeds.length) memory(seeds(t - 1)) = (t, t)
+    var lastSeen = seeds.last
+    for (t <- seeds.length + 1 to limit) {
+      memory(lastSeen) match {
+        case (t0, t1) if t0 == t1 => lastSeen = 0
+        case (t0, t1) => lastSeen = memory(lastSeen)._2 - memory(lastSeen)._1
+      }
+      if (memory(lastSeen) == (-1, -1)) memory(lastSeen) = (t, t)
+      else memory(lastSeen) = (memory(lastSeen)._2, t)
+    }
+    lastSeen
   }
 }
-
-trait Part1 extends Common {
-  def part1(instructions: IndexedSeq[String]): Long = {
-    val memory = allocateMem()
-
-    instructions.foldLeft((0L, 0L)) {
-      case (_, Mask(m)) => parseMask(m)()
-      case ((and, or), Write(addr, value)) => memory(addr.toInt) = (value.toLong & and) | or; (and, or)
-    }
-
-    memory.values.sum
-  }
-
-  // return (and, or)
-  private final def parseMask(maskStr: String, shift: Int = 0)(and: Long = MASK, or: Long = 0): (Long, Long) = {
-    if (maskStr.isEmpty) (and, or)
-    else {
-      val f = parseMask(maskStr.init, shift + 1) _
-      maskStr.last match {
-        case 'X' => f(and, or)
-        case '0' => f(and & ~(1L << shift), or)
-        case '1' => f(and, or | (1L << shift))
-      }
-    }
-  }
-
-}
-
-trait Part2 extends Common {
-  private type Op = Long => Long
-
-  def part2(instructions: IndexedSeq[String]): Long = {
-    val memory = allocateMem()
-
-    instructions.foldLeft((0L, 0L)) {
-      case (_, Mask(m)) => parseMask(m)()
-      case ((float, or), Write(addr, value)) =>
-        floatOps(or, float).map(_ (addr.toLong)).foreach(addr => memory(addr) = value.toLong); (float, or)
-    }
-
-    memory.values.sum
-  }
-
-  private def floatOps(orMask: Long, float: Long): Seq[Op] = {
-    // which bits (e.g, 1, 2, 32, etc.) represent floats?
-    val bits = (0 until 36).filter(bit => (float & (1L << bit)) != 0)
-
-    // for each floating bit, we need to generate the rules to transform
-    // an address into 'n' addresses (for 'n' floating bits). To do that, we build
-    // up a sequence of lambda functions, 2 for each bit (one setting the bit on, one
-    // setting it off). We then compose all those functions together and return them
-    // together. We then push the address through each composed function, and set
-    // the value at that address.
-    // Note that each address is 'or'd with orMask to apply the 2nd rule ("1's are overwritten") (masks default arg)
-    // The first rule ("0's are unchanged") is trivially satisfied.
-    @tailrec
-    def recurse(bits: Seq[Int], masks: Seq[Op] = Seq(_ | orMask, _ | orMask)): Seq[Op] = {
-      if (bits.isEmpty) masks
-      else {
-        def or: Long => Long = _ | (1L << bits.head)
-
-        def and: Long => Long = _ & ~(1L << bits.head)
-
-        recurse(bits.tail, masks.flatMap(op => Seq(op compose or, op compose and)))
-      }
-    }
-
-    recurse(bits)
-  }
-
-  // return (float, or)
-  // Given a mask string, returns the "float" bits and the "or" bits.
-  // For the float bits, we need to generate values at those bits that are both a 0 and a 1.
-  // The or mask satisfies rule #2, "1s are overwritten". Rule #1 is trivial ("no change")
-  private final def parseMask(maskStr: String, shift: Int = 0)(float: Long = 0, or: Long = 0): (Long, Long) = {
-    if (maskStr.isEmpty) (float, or)
-    else {
-      val f = parseMask(maskStr.init, shift + 1) _
-      maskStr.last match {
-        case 'X' => f(float | (1L << shift), or)
-        case '0' => f(float, or)
-        case '1' => f(float, or | (1L << shift))
-      }
-    }
-  }
-}
-
-
-trait Day14 extends Part1 with Part2
--- src/test/scala/y2020/Day15Spec.scala
+++ src/test/scala/y2020/Day15Spec.scala
@@ -4,33 +4,35 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should._
 import util.Loader
 
-class Day14Spec extends AnyFlatSpec with Matchers with Day14 {
+class Day15Spec extends AnyFlatSpec with Matchers with Day15 {
 
-  lazy val input: IndexedSeq[String] = Loader(this, "day14.txt").toIndexedSeq
+  lazy val input: IndexedSeq[String] = Loader(this, "day15.txt").toIndexedSeq
 
   it should "solve part 1 test" in {
-    part1(
-      """
-        |mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-        |mem[8] = 11
-        |mem[7] = 101
-        |mem[8] = 0""".stripMargin.trim.linesIterator.toIndexedSeq) shouldBe 165L
+    solve("0,3,6",2020 ) shouldBe 436
+    solve("1,3,2", 2020 ) shouldBe 1
+    solve("2,1,3", 2020 ) shouldBe 10
+    solve("1,2,3", 2020) shouldBe 27
+    solve("2,3,1", 2020) shouldBe 78
+    solve("3,2,1", 2020) shouldBe 438
+    solve("3,1,2", 2020) shouldBe 1836
   }
 
   it should "solve part 1" in {
-    part1(Loader(this, "day14.txt").toIndexedSeq) shouldBe 15514035145260L
+    solve(Loader(this, "day15.txt").mkString, 2020) shouldBe 1522
   }
 
   it should "solve part 2 test" in {
-    part2(
-      """
-        |mask = 000000000000000000000000000000X1001X
-        |mem[42] = 100
-        |mask = 00000000000000000000000000000000X0XX
-        |mem[26] = 1""".stripMargin.trim.linesIterator.toIndexedSeq) shouldBe 208L
+    solve("0,3,6",30000000 ) shouldBe 175594
+    solve("1,3,2", 30000000 ) shouldBe 2578
+    solve("2,1,3", 30000000 ) shouldBe 3544142
+    solve("1,2,3", 30000000) shouldBe 261214
+    solve("2,3,1", 30000000) shouldBe 6895259
+    solve("3,2,1", 30000000) shouldBe 18
+    solve("3,1,2", 30000000) shouldBe 362
   }
 
   it should "solve part 2" in {
-    part2(Loader(this, "day14.txt").toIndexedSeq) shouldBe 3926790061594L
+    solve(Loader(this, "day15.txt").mkString, 30000000) shouldBe 18234
   }
 }
