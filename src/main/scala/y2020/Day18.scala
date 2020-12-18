ed3faf6d7e282b291f0edbf126d92faa2c749f59
Eric Bowman <ebowman@boboco.ie>
2020-12-18 08:26:02 +0100

    polishing

--- src/main/scala/y2020/Day18.scala
+++ src/main/scala/y2020/Day18.scala
@@ -1,21 +1,28 @@
 package y2020
 
+import scala.util.matching.Regex
 import scala.util.parsing.combinator.JavaTokenParsers
 
 trait Day18 extends JavaTokenParsers {
 
-  trait Part1Parser extends JavaTokenParsers {
+  val optWsRe: Regex = """\s*""".r
+
+  trait Shared extends JavaTokenParsers {
+
+    override def skipWhitespace = false
+
+    def optWs[T](p: => Parser[T]): Parser[T] = optWsRe ~> p <~ optWsRe
+
     def number: Parser[Long] = wholeNumber ^^ (_.toLong)
 
-    def factor: Parser[Long] = number | "(" ~> expr <~ ")"
+    def factor: Parser[Long] = number | optWs("(") ~> expr <~ optWs(")")
 
-    def plusTerm: Parser[Long] = factor ~ "+" ~ factor ^^ { case x ~ _ ~ y => x + y }
+    def expr: Parser[Long]
+  }
 
-    def timesTerm: Parser[Long] = factor ~ "*" ~ factor ^^ { case x ~ _ ~ y => x * y }
+  trait Part1Parser extends Shared {
 
-    def term: Parser[Long] = factor | plusTerm | timesTerm
-
-    def expr: Parser[Long] = term ~ rep(("+" ~ term) | ("*" ~ term)) ^^ {
+    def expr: Parser[Long] = factor ~ rep((optWs("+") ~ factor) | (optWs("*") ~ factor)) ^^ {
       case num ~ list => list.foldLeft(num) {
         case (x, "+" ~ y) => x + y
         case (x, "*" ~ y) => x * y
@@ -23,26 +30,21 @@ trait Day18 extends JavaTokenParsers {
     }
   }
 
-
   def part1(input: IndexedSeq[String]): Long = {
     val p = new Part1Parser {}
-    input.map(line => p.parseAll(p.expr, line).get.toLong).sum
+    input.map(line => p.parseAll(p.expr, line).get).sum
   }
 
-  trait Part2Parser extends JavaTokenParsers {
-    def number: Parser[Long] = wholeNumber ^^ (_.toLong)
+  trait Part2Parser extends Shared {
 
-    def factor: Parser[Long] = number | "(" ~> expr <~ ")"
+    def term: Parser[Long] = factor ~ rep(optWs("+") ~> factor) ^^ { case x ~ y => y.foldLeft(x)(_ + _) }
 
-    def plusTerm: Parser[Long] = factor ~ rep("+" ~> factor) ^^ { case x ~ y => y.foldLeft(x)(_ + _) }
-
-    def term: Parser[Long] = plusTerm | factor
-
-    def expr: Parser[Long] = term ~ rep(("*" ~> term)) ^^ { case x ~ y => y.foldLeft(x)(_ * _) }
+    def expr: Parser[Long] = term ~ rep((optWs("*") ~> term)) ^^ { case x ~ y => y.foldLeft(x)(_ * _) }
   }
+
   def part2(input: IndexedSeq[String]): Long = {
     val p = new Part2Parser {}
-    input.map(line => p.parseAll(p.expr, line).get.toLong).sum
+    input.map(line => p.parseAll(p.expr, line).get).sum
   }
 }
 
