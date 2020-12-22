bca211d5642db48121c2d66476d62223dc6d0f81
Eric Bowman <ebowman@boboco.ie>
2020-12-22 13:23:59 +0100

    polish

--- src/test/scala/y2020/Day22Spec.scala
+++ src/test/scala/y2020/Day22Spec.scala
@@ -4,7 +4,6 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should._
 import util.Loader
 
-
 class Day22Spec extends AnyFlatSpec with Matchers with Day22 {
 
   lazy val testInput: IndexedSeq[String] = Loader.is(this, "day22.test.txt")
@@ -20,18 +19,6 @@ class Day22Spec extends AnyFlatSpec with Matchers with Day22 {
 
   it should "solve part 2 test" in {
     part2(testInput) shouldBe 291
-  }
-
-  it should "not be pathological" in {
-    part2(
-      """|Player 1:
-         |43
-         |19
-         |
-         |Player 2:
-         |2
-         |29
-         |14""".stripMargin.trim.linesIterator.toIndexedSeq) shouldBe 105
   }
 
   it should "solve part 2" in {
