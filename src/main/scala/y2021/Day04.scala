b0453f87affd56aa805f2d5e41ee3fb5eeacc79f
Eric Bowman <ebowman@boboco.ie>
2021-12-11 15:18:33 +0100

    2021.04.1

--- src/main/scala/y2021/Day04.scala
+++ src/main/scala/y2021/Day04.scala
@@ -4,22 +4,6 @@ trait Day04 {
   case class Board(x: Array[Int] = Array.fill(100)(-1),
                    y: Array[Int] = Array.fill(100)(-1),
                    marks: Array[Boolean] = Array.fill(100)(false)) {
-
-    override def toString: String = {
-      val b = new StringBuilder
-      for (y <- 0 until 5) {
-        for (x <- 0 until 5) {
-          val s = valOf(x, y).get
-          if (isMarked((x, y)))
-            b.append("*").append(valOf(x, y).get).append("*").append(" ")
-          else
-            b.append(valOf(x, y).get).append(" ")
-        }
-        b.append("\n")
-      }
-      b.toString
-    }
-
     def put(c: (Int, Int), v: Int): Unit = {
       x(v) = c._1
       y(v) = c._2
@@ -31,60 +15,67 @@ trait Day04 {
 
     def isMarked(c: (Int, Int)) = valOf(c).map(marks).getOrElse(false)
 
-    def valOf(c: (Int, Int)): Option[Int] =
-      (0 until 100).find(i => x(i) == c._1 && y(i) == c._2)
+    def valOf(c: (Int, Int)): Option[Int] = (0 until 100).find(i => x(i) == c._1 && y(i) == c._2)
 
-    def mark(m: Int) = marks(m) = true
+    def mark(m: Int): Unit = marks(m) = true
 
-    def sumUnmarked: Int = {
-      (0 until 100).filter(i => x(i) != -1).filterNot(isMarked).sum
-    }
+    def sumUnmarked: Int = (0 until 100).filter(i => x(i) != -1).filterNot(isMarked).sum
 
     def play(v: Int): Boolean = {
-      val c = coordOf(v)
-      if (c == (-1, -1)) false
-      else {
-        mark(v)
-      }
+      if (coordOf(v) != (-1, -1)) mark(v)
       isBingo
     }
 
-    def isBingo: Boolean = {
-      var found = false
-      for (x <- 0 until 5) {
-        if (!found) {
-          val c = (0 until 5).map(y => (x, y)).count(isMarked)
-          found = c == 5
-          //found = (0 until y.length).map(y => (x, y)).count(isMarked) == y.length
-        }
-      }
-      if (!found) {
-        for (y <- 0 until 5) {
-          if (!found) {
-            val c = (0 until 5).map(x => (x, y)).count(isMarked)
-            found = c == 5
-            //found = (0 until x.length).map(x => (x, y)).count(isMarked) == x.length
-          }
-        }
-      }
-      found
-    }
+    def isBingo: Boolean = //noinspection DuplicatedCode
+      ((0 until 5).exists(x => (0 until 5).map(y => (x, y)).count(isMarked) == 5)) ||
+        (0 until 5).exists(y => (0 until 5).map(x => (x, y)).count(isMarked) == 5)
   }
 
   def loadBoard(lines: Iterable[String]): (Seq[Int], Set[Board]) = {
     val iter = lines.iterator
-    val plays: Seq[Int] = iter.next().split(",").map(_.toInt)
+    val plays = iter.next().split(",").map(_.toInt)
     var set = Set[Board]()
     while (iter.hasNext) {
       iter.next()
-      val b = Board()
       val bits = iter.take(5).flatMap(line =>
-        line.split("\\s+")).filter(_.trim.length > 0).map(_.toInt).toIndexedSeq
-      for (y <- 0 until 5; x <- 0 until 5) {
-        b.put((x, y), bits(y * 5 + x))
-      }
-      set = set + b
+        line.split("""\s+""")).filter(_.trim.nonEmpty).map(_.toInt).toIndexedSeq
+      val b = Board()
+      for (y <- 0 until 5; x <- 0 until 5) b.put((x, y), bits(y * 5 + x))
+      set += b
     }
     (plays, set)
+  }
+
+  def solve1(input: Seq[String]): Int = {
+    val (plays, boards) = loadBoard(input)
+    var done = false
+    var score = 0
+    val playIter = plays.iterator
+    while (!done) {
+      val next = playIter.next()
+      for (b <- boards if b.play(next)) {
+        done = true
+        score = b.sumUnmarked * next
+      }
+    }
+    score
+  }
+
+  def solve2(input: Seq[String]): Int = {
+    var (plays, boards) = loadBoard(input)
+    var done = false
+    var score = 0
+    val playIter = plays.iterator
+    while (!done) {
+      var next = playIter.next()
+      if (boards.size == 1) {
+        val b = boards.head
+        while (!done) {
+          if (b.play(next)) done = true else next = playIter.next()
+        }
+        score = b.sumUnmarked * next
+      } else for (b <- boards if b.play(next)) boards = boards - b
+    }
+    score
   }
 }
--- src/test/scala/y2021/Day04Spec.scala
+++ src/test/scala/y2021/Day04Spec.scala
@@ -9,84 +9,18 @@ class Day04Spec extends AnyFlatSpec with Matchers with Day04 {
   lazy val input = Loader(this, "day04.txt").toSeq
 
   "Day04" should "pass the part 1 tests" in {
-    val (plays, boards) = loadBoard(testInput)
-    var done = false
-    var score = 0
-    val playIter = plays.iterator
-    while (!done) {
-      val next = playIter.next()
-      for (b <- boards) {
-        if (b.play(next)) {
-          done = true
-          score = b.sumUnmarked * next
-        }
-      }
-    }
-    score shouldBe 4512
+    solve1(testInput) shouldBe 4512
   }
 
   it should "pass part 1" in {
-    val (plays, boards) = loadBoard(input)
-    var done = false
-    var score = 0
-    val playIter = plays.iterator
-    while (!done) {
-      val next = playIter.next()
-      for (b <- boards) {
-        if (b.play(next)) {
-          done = true
-          score = b.sumUnmarked * next
-        }
-      }
-    }
-    score shouldBe 5685
+    solve1(input) shouldBe 5685
   }
 
   it should "pass part 2 test" in {
-    var (plays, boards) = loadBoard(testInput)
-    var done = false
-    var score = 0
-    val playIter = plays.iterator
-    while (!done) {
-      var next = playIter.next()
-      if (boards.size == 1) {
-        val b = boards.head
-        while (!done) {
-          if (b.play(next)) done = true else next = playIter.next()
-        }
-        score = b.sumUnmarked * next
-      } else {
-        for (b <- boards) {
-          if (b.play(next)) {
-            boards = boards - b
-          }
-        }
-      }
-    }
-    score shouldBe 1924
+    solve2(testInput) shouldBe 1924
   }
 
   it should "pass part 2" in {
-    var (plays, boards) = loadBoard(input)
-    var done = false
-    var score = 0
-    val playIter = plays.iterator
-    while (!done) {
-      var next = playIter.next()
-      if (boards.size == 1) {
-        val b = boards.head
-        while (!done) {
-          if (b.play(next)) done = true else next = playIter.next()
-        }
-        score = b.sumUnmarked * next
-      } else {
-        for (b <- boards) {
-          if (b.play(next)) {
-            boards = boards - b
-          }
-        }
-      }
-    }
-    score shouldBe 21070
+    solve2(input) shouldBe 21070
   }
 }
