2db81dc99ba36432764962cc8c658736acc98662
Eric Bowman <ebowman@boboco.ie>
2021-12-11 14:46:20 +0100

    2021.03.1

--- src/main/scala/y2021/Day03.scala
+++ src/main/scala/y2021/Day03.scala
@@ -1,58 +1,38 @@
 package y2021
 
-trait Day03 {
+import scala.annotation.tailrec
 
-  def toBinary(i: String, x: Int = 0): Int = {
+trait Day03 {
+  @tailrec final def toBinary(i: String, x: Int = 0): Int =
     if (i.isEmpty) x
-    else if (i(0) == '1') toBinary(i.tail, (x << 1) + 1)
-    else toBinary(i.tail, x << 1)
-  }
+    else toBinary(i.tail, (x << 1) + (i(0) - '0'))
 
   def solve1(input: Seq[String]): Int = {
-    val bits = input.head.length
-    val accum = Array.fill[Int](bits)(0)
-    for (i <- input; j <- 0 until i.length)
-      i(j) match {
-        case '0' => accum(j) = accum(j) - 1
-        case '1' => accum(j) = accum(j) + 1
-      }
-
-    def recurse(i: Array[Int], b: StringBuilder = new StringBuilder): String = {
+    @tailrec def recurse(i: Array[Int], b: StringBuilder = new StringBuilder): String = {
       if (i.isEmpty) b.toString()
-      else {
-        if (i(0) > 0) recurse(i.tail, b.append(("1")))
-        else recurse(i.tail, b.append("0"))
-      }
+      else recurse(i.tail, b.append(if (i(0) > 0) '1' else '0'))
     }
 
-    def flip(s: String): String = {
-      def r(x: String, b: StringBuilder = new StringBuilder): String = {
-        if (x.isEmpty) b.toString()
-        else if (x(0) == '1') r(x.substring(1), b.append("0"))
-        else r(x.substring(1), b.append("1"))
-      }
-
-      r(s)
+    @tailrec def flip(s: String, b: StringBuilder = new StringBuilder): String = {
+      if (s.isEmpty) b.toString()
+      else flip(s.tail, b.append(('a' - s(0)).toChar))
     }
 
+    val accum = Array.fill[Int](input.head.length)(0)
+    for (line <- input; i <- line.indices) accum(i) += 2 * (line(i) - '0') - 1
     val a = recurse(accum)
-    val x = toBinary(a)
-    val b = flip(a)
-    val y = toBinary(b)
-    x * y
+    toBinary(a) * toBinary(flip(a))
   }
 
-  def solve2(s: Seq[String], c: Char, i: Int = 0): String = {
-    def counts(s: Seq[String], i: Int, c: Char): Int = s.map(_ (i)).count(_ == c)
-
-    if (s.length == 1) s.head
+  @tailrec final def solve2(s: Seq[String], c: Char, i: Int = 0): Int =
+    if (s.length == 1) toBinary(s.head)
     else {
-      val ones = counts(s, i, c)
-      val zeros = s.length - ones
-      if ((c == '1' && ones >= zeros) || (c == '0' && ones <= zeros))
-        solve2(s.filter(_ (i) == c), c, i + 1)
-      else
-        solve2(s.filter(_ (i) != c), c, i + 1)
+      val ones = 2 * s.map(_ (i)).count(_ == c)
+      solve2(s.filter {
+        if (c == '1' && ones >= s.length || c == '0' && ones <= s.length)
+          _ (i) == c
+        else
+          _ (i) != c
+      }, c, i + 1)
     }
-  }
 }
--- src/test/scala/y2021/Day03Spec.scala
+++ src/test/scala/y2021/Day03Spec.scala
@@ -30,14 +30,10 @@ class Day03Spec extends AnyFlatSpec with Matchers with Day03 {
     solve1(inputs) shouldBe 1082324
   }
   it should "test part 2" in {
-    val ones = solve2(testInput, '1')
-    val zeros = solve2(testInput, '0')
-    (toBinary(ones) * toBinary(zeros)) shouldBe 230
+    (solve2(testInput, '1') * solve2(testInput, '0')) shouldBe 230
   }
 
   it should "solve part 2" in {
-    val ones = solve2(inputs, '1')
-    val zeros = solve2(inputs, '0')
-    (toBinary(ones) * toBinary(zeros)) shouldBe 1353024
+    solve2(inputs, '1') * solve2(inputs, '0') shouldBe 1353024
   }
 }
