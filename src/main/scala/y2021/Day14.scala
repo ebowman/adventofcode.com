48851ac027073b45044390887e7d4b61562f385d
Eric Bowman <ebowman@boboco.ie>
2021-12-14 13:58:58 +0100

    2021.14.3

--- src/main/scala/y2021/Day14.scala
+++ src/main/scala/y2021/Day14.scala
@@ -1,9 +1,6 @@
 package y2021
 
-import scala.annotation.tailrec
-
 trait Day14 {
-
   def loadRules(input: Seq[String]): (String, Map[String, String]) = {
     val seed = input.head
     val rules = input.tail.tail.map { rule =>
@@ -14,47 +11,24 @@ trait Day14 {
   }
 
   def solve(input: Seq[String], count: Int): Long = {
-    type CMap = Map[String, Long]
+    def map() = Map[String, Long]().withDefaultValue(0L)
 
     val (seed, rules) = loadRules(input)
-
-    @tailrec def compute(tupleFrequencyCounts: CMap, count: Int): CMap = {
-      if (count == 0) tupleFrequencyCounts
-      else {
-        def recurFreq(c: CMap, f: CMap = Map().withDefaultValue(0L)): CMap = {
-          if (c.isEmpty) f
-          else {
-            val (k, v) = c.head
-            val k1 = k(0) + rules(k)
-            val k2 = rules(k) + k(1)
-            recurFreq(c.tail, f + (k1 -> (f(k1) + v)) + (k2 -> (f(k2) + v)))
-          }
+    val perLetterCounts = {
+      val plc = (1 to count).foldLeft(seed.sliding(2).foldLeft(map()) { case (fc, r) =>
+        fc + (r -> (fc(r) + 1L))
+      }) { case (tfc, _) =>
+        tfc.foldLeft(map()) { case (map, (k, v)) =>
+          val k1 = k(0) + rules(k)
+          val k2 = rules(k) + k(1)
+          map + (k1 -> (map(k1) + v)) + (k2 -> (map(k2) + v))
         }
-        compute(recurFreq(tupleFrequencyCounts), count - 1)
-      }
-    }
-
-    @tailrec def slideFreq(pairs: Iterator[String], fc: CMap = Map().withDefaultValue(0L)): CMap = {
-      if (pairs.isEmpty) fc
-      else {
-        val r = pairs.next()
-        slideFreq(pairs, fc + (r -> (fc(r) + 1L)))
-      }
-    }
-
-    val pairFrequencyCounts = slideFreq(seed.sliding(2))
-    val finalPairFrequencyCounts = compute(pairFrequencyCounts, count)
-
-    @tailrec def recurseCount(keys: Iterable[String], plc: CMap = Map().withDefaultValue(0L)): CMap = {
-      if (keys.isEmpty) plc + (s"${seed.last}" -> (plc(s"${seed.last}") + 1))
-      else {
-        val letter = keys.head
+      }.foldLeft(map()) { case (plc, (letter, value)) =>
         val k = s"${letter.head}"
-        recurseCount(keys.tail, plc + (k -> (plc(k) + finalPairFrequencyCounts(letter))))
+        plc + (k -> (plc(k) + value))
       }
+      plc + (s"${seed.last}" -> (plc(s"${seed.last}") + 1))
     }
-
-    val perLetterCounts = recurseCount(finalPairFrequencyCounts.keys)
     perLetterCounts.maxBy(_._2)._2 - perLetterCounts.minBy(_._2)._2
   }
 }
