1dd2302ae9d9d8a790b3d816c29ff923f96be590
Eric Bowman <ebowman@boboco.ie>
2021-12-21 16:58:09 +0100

    2015.24.1

--- src/main/scala/y2015/Day24.scala
+++ src/main/scala/y2015/Day24.scala
@@ -4,8 +4,8 @@ trait Day24 {
   def solve(input: Seq[String], parts: Int = 3): BigInt = {
     val packages = input.map(_.toInt)
     val weight = packages.sum / parts
-    packages.combinations((1 to packages.size).map { n =>
-      packages.combinations(n).filter(_.sum == weight).minByOption(_.size).map(_.size).getOrElse(Int.MaxValue)
-    }.min).filter(_.sum == weight).map(_.map(BigInt.apply)).map(_.product).min
+    (1 to packages.size).find(n => packages.combinations(n).exists(_.sum == weight)).map { min =>
+      packages.combinations(min).filter(_.sum == weight).map(_.map(BigInt.apply)).map(_.product).min
+    }.get
   }
 }
