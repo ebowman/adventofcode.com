83b204d6467489ce8d5292efd06e3ff5b0b64d34
Eric Bowman <ebowman@boboco.ie>
2021-12-22 19:53:16 +0100

    2021.22.3

--- src/main/scala/y2021/Day22.scala
+++ src/main/scala/y2021/Day22.scala
@@ -17,33 +17,34 @@ trait Day22 {
     core.count(_._2 == true)
   }
 
-  case class Cube(xMin: Int, xMax: Int, yMin: Int, yMax: Int, zMin: Int, zMax: Int) {
-    def intersect(that: Cube): Option[Cube] = {
-      val c = Cube(max(xMin, that.xMin), min(xMax, that.xMax),
-        max(yMin, that.yMin), min(yMax, that.yMax),
-        max(zMin, that.zMin), min(zMax, that.zMax))
-      if (c.nonEmpty) Some(c) else None
-    }
+  case class Cube(x1: Int, x2: Int, y1: Int, y2: Int, z1: Int, z2: Int) {
+    def intersect(t: Cube): Option[Cube] =
+      Cube(max(x1, t.x1), min(x2, t.x2), max(y1, t.y1), min(y2, t.y2), max(z1, t.z1), min(z2, t.z2)).nonEmpty
 
-    def nonEmpty: Boolean = xMin <= xMax && yMin <= yMax && zMin <= zMax
+    def nonEmpty: Option[Cube] = if (x1 <= x2 && y1 <= y2 && z1 <= z2) Some(this) else None
 
-    def volume(sign: Long): Long = (xMax - xMin + 1L) * (yMax - yMin + 1L) * (zMax - zMin + 1L) * sign
+    def volume(sign: Long): Long = (x2 - x1 + 1L) * (y2 - y1 + 1L) * (z2 - z1 + 1L) * sign
   }
 
-  private def listToTuple[A](seq: Seq[A]): Product =
-    Class.forName("scala.Tuple" + seq.size).getConstructors.apply(0).newInstance(seq: _*).asInstanceOf[Product]
+  object CubeFactory {
+    type tuple = (Int, Int, Int, Int, Int, Int)
+
+    private def listToTuple[A](seq: Seq[A]): Product =
+      Class.forName("scala.Tuple" + seq.size).getConstructors.apply(0).newInstance(seq: _*).asInstanceOf[Product]
+
+    def apply(line: String): Cube =
+      Cube.tupled(listToTuple("""-?\d+""".r.findAllIn(line).map(_.toInt).toSeq).asInstanceOf[tuple])
+  }
 
   def map(): Map[Cube, Int] = Map.empty.withDefaultValue(0)
 
   @tailrec final def solve2(input: Seq[String], cubes: Map[Cube, Int] = map()): Long = {
-    type tuple = (Int, Int, Int, Int, Int, Int)
     if (input.isEmpty) cubes.map { case (cube, sign) => cube.volume(sign) }.sum
     else {
-      val cube = Cube.tupled(listToTuple("""-?\d+""".r.findAllIn(input.head).map(_.toInt).toSeq).asInstanceOf[tuple])
-      val accum = cubes.foldLeft(map() ++ Seq(cube -> 1).filter(_ => input.head.startsWith("on"))) {
+      val cube = CubeFactory(input.head)
+      solve2(input.tail, cubes.foldLeft(map() ++ Seq(cube -> 1).filter(_ => input.head.startsWith("on"))) {
         case (accum, (that, sign)) => cube.intersect(that).map(c => accum + (c -> (accum(c) - sign))).getOrElse(accum)
-      }
-      solve2(input.tail, accum.foldLeft(cubes) { case (cubes, (c, sign)) => cubes + (c -> (cubes(c) + sign)) })
+      }.foldLeft(cubes) { case (cubes, (c, sign)) => cubes + (c -> (cubes(c) + sign)) })
     }
   }
 }
