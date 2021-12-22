8d87defc4ea635c9bc7fe37f068049d1226e1b04
Eric Bowman <ebowman@boboco.ie>
2021-12-22 19:30:44 +0100

    2021.22.2

--- src/main/scala/y2021/Day22.scala
+++ src/main/scala/y2021/Day22.scala
@@ -1,45 +1,49 @@
 package y2021
 
+import math.{max, min}
 import scala.collection.mutable
-import math.{min, max}
+import scala.annotation.tailrec
 
 trait Day22 {
-
   def solve1(input: Seq[String]): Int = {
     val core = mutable.Map[(Int, Int, Int), Boolean]().withDefaultValue(false)
     input.foreach { line =>
-      val on = line.startsWith("on")
-      val Array(xmin, xmax, ymin, ymax, zmin, zmax) = """-?\d+""".r.findAllIn(line).toArray.map(_.toInt)
-      for {
-        x <- max(-50, xmin) to min(50, xmax)
-        y <- max(-50, ymin) to min(50, ymax)
-        z <- max(-50, zmin) to min(50, zmax)
-      } {
-        if (on) core((x, y, z)) = true
-        else core.remove((x, y, z))
-      }
+      val Array(xMin, xMax, yMin, yMax, zMin, zMax) = """-?\d+""".r.findAllIn(line).toArray.map(_.toInt)
+      for {x <- max(-50, xMin) to min(50, xMax)
+           y <- max(-50, yMin) to min(50, yMax)
+           z <- max(-50, zMin) to min(50, zMax)}
+        if (line.startsWith("on")) core((x, y, z)) = true else core.remove((x, y, z))
     }
     core.count(_._2 == true)
   }
 
-  def solve2(input: Seq[String]): Long = {
-    val cubes = mutable.Map[(Int, Int, Int, Int, Int, Int), Long]().withDefaultValue(0L)
-    input.foreach { line =>
-      val Seq(minX, maxX, minY, maxY, minZ, maxZ) = """-?\d+""".r.findAllIn(line).map(_.toInt).toSeq
-      val sign = if (line.startsWith("on")) 1 else -1
-      val thisCube = mutable.Map[(Int, Int, Int, Int, Int, Int), Long]().withDefaultValue(0)
-      for (((minX2, maxX2, minY2, maxY2, minZ2, maxZ2), sign2) <- cubes) yield {
-        val (x1, y1, z1) = (math.max(minX, minX2), math.max(minY, minY2), math.max(minZ, minZ2))
-        val (x2, y2, z2) = (math.min(maxX, maxX2), math.min(maxY, maxY2), math.min(maxZ, maxZ2))
-        if (x1 <= x2 && y1 <= y2 && z1 <= z2) {
-          thisCube((x1, x2, y1, y2, z1, z2)) -= sign2
-        }
-      }
-      if (sign == 1) thisCube((minX, maxX, minY, maxY, minZ, maxZ)) += sign
-      thisCube.foreach { case (k, v) => cubes(k) += v }
+  case class Cube(xMin: Int, xMax: Int, yMin: Int, yMax: Int, zMin: Int, zMax: Int) {
+    def intersect(that: Cube): Option[Cube] = {
+      val c = Cube(max(xMin, that.xMin), min(xMax, that.xMax),
+        max(yMin, that.yMin), min(yMax, that.yMax),
+        max(zMin, that.zMin), min(zMax, that.zMax))
+      if (c.nonEmpty) Some(c) else None
     }
-    cubes.map { case ((minX, maxX, minY, maxY, minZ, maxZ), sign) =>
-      (maxX - minX + 1L) * (maxY - minY + 1L) * (maxZ - minZ + 1L) * sign
-    }.sum
+
+    def nonEmpty: Boolean = xMin <= xMax && yMin <= yMax && zMin <= zMax
+
+    def volume(sign: Long): Long = (xMax - xMin + 1L) * (yMax - yMin + 1L) * (zMax - zMin + 1L) * sign
+  }
+
+  private def listToTuple[A](seq: Seq[A]): Product =
+    Class.forName("scala.Tuple" + seq.size).getConstructors.apply(0).newInstance(seq: _*).asInstanceOf[Product]
+
+  def map(): Map[Cube, Int] = Map.empty.withDefaultValue(0)
+
+  @tailrec final def solve2(input: Seq[String], cubes: Map[Cube, Int] = map()): Long = {
+    type tuple = (Int, Int, Int, Int, Int, Int)
+    if (input.isEmpty) cubes.map { case (cube, sign) => cube.volume(sign) }.sum
+    else {
+      val cube = Cube.tupled(listToTuple("""-?\d+""".r.findAllIn(input.head).map(_.toInt).toSeq).asInstanceOf[tuple])
+      val accum = cubes.foldLeft(map() ++ Seq(cube -> 1).filter(_ => input.head.startsWith("on"))) {
+        case (accum, (that, sign)) => cube.intersect(that).map(c => accum + (c -> (accum(c) - sign))).getOrElse(accum)
+      }
+      solve2(input.tail, accum.foldLeft(cubes) { case (cubes, (c, sign)) => cubes + (c -> (cubes(c) + sign)) })
+    }
   }
 }
