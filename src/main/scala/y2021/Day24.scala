a4286996aa6189015bc7954aa896c12f7b9519ea
Eric Bowman <ebowman@boboco.ie>
2021-12-26 10:32:52 +0100

    2021.24.2

--- src/main/scala/y2021/Day24.scala
+++ src/main/scala/y2021/Day24.scala
@@ -1,35 +1,30 @@
 package y2021
 
+import scala.annotation.tailrec
+
 trait Day24 {
   def solve(input: Seq[String]): (Long, Long) = {
-    import collection.mutable
-    val blocks = input.mkString("\n").split("inp w\n").toSeq.tail.map(_.split("\n").toSeq)
-    val max = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    val min = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    val stack = mutable.Stack[(Int, Int)]()
-    for {
-      i <- blocks.indices
-      block = blocks(i)
-    } {
-      block(3) match {
-        case "div z 1" =>
-          stack.push((i, block(14).split(' ').last.toInt))
+
+    @tailrec def recurse(blocks: Seq[Seq[String]],
+                         working: List[(Int, Int)] = Nil,
+                         i: Int = 0,
+                         min: Seq[Int] = Iterator.iterate(0, 14)(_ => 0).toSeq,
+                         max: Seq[Int] = Iterator.iterate(0, 14)(_ => 0).toSeq): (Long, Long) = {
+      if (blocks.isEmpty) (max.mkString.toLong, min.mkString.toLong)
+      else blocks.head(3) match {
+        case "div z 1" => recurse(blocks.tail, (i, blocks.head(14).split(' ').last.toInt) :: working, i + 1, min, max)
         case "div z 26" =>
-          val (j, x) = stack.pop()
-          val diff = x + block(4).split(" ").last.toInt
-          if (diff < 0) {
-            max(j) = 9
-            max(i) = 9 + diff
-            min(j) = 1 - diff
-            min(i) = 1
-          } else {
-            max(i) = 9
-            max(j) = 9 - diff
-            min(i) = 1 + diff
-            min(j) = 1
-          }
+          val (j, x) = working.head
+          val diff = x + blocks.head(4).split(" ").last.toInt
+          if (diff < 0) recurse(blocks.tail, working.tail, i + 1,
+            min.updated(j, 9).updated(i, 9 + diff),
+            max.updated(j, 1 - diff).updated(i, 1))
+          else recurse(blocks.tail, working.tail, i + 1,
+            min.updated(i, 9).updated(j, 9 - diff),
+            max.updated(i, 1 + diff).updated(j, 1))
       }
     }
-    (min.mkString.toLong, max.mkString.toLong)
+
+    recurse(input.mkString("\n").split("inp w\n").toSeq.tail.map(_.split("\n").toSeq))
   }
 }
