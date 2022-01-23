bd819a455ee20865834ff9a278194adde3d61173
Eric Bowman <ebowman@boboco.ie>
2022-01-23 20:20:05 +0100

    optimize 2015.09

--- src/main/scala/y2015/Day09.scala
+++ src/main/scala/y2015/Day09.scala
@@ -1,29 +1,41 @@
 package y2015
 
+import scala.collection.mutable
 import scala.util.parsing.combinator.RegexParsers
 
-trait Day09 extends RegexParsers {
+trait Day09 {
+  class Solver(input: Seq[String]) {
+    lazy val minMax: (Int, Int) = {
+      val parser = new GraphParser(input)
+      parser.places.toSeq.permutations.map { path =>
+        path.sliding(2).map(edge => parser.paths(edge.head)(edge(1))).sum
+      }.foldLeft((Int.MaxValue, Int.MinValue)) {
+        case ((min, max), v) => (math.min(min, v), math.max(max, v))
+      }
+    }
 
-  import scala.collection.mutable
+    class GraphParser(input: Seq[String]) extends RegexParsers {
+      val places: mutable.Set[String] = mutable.Set[String]()
+      val paths: mutable.Map[String, mutable.Map[String, Int]] =
+        new mutable.HashMap[String, mutable.Map[String, Int]]() {
+          override def default(key: String): mutable.Map[String, Int] =
+            put(key, mutable.Map[String, Int]())
+            apply(key)
+        }
+      input.foreach(line => parseAll(edge, line))
 
-  val places = new mutable.HashSet[String]()
-  val paths = new mutable.HashMap[String, mutable.HashMap[String, Int]]()
+      def edge: Parser[Unit] = place ~ ("to" ~> place) ~ ("=" ~> distance) ^^ {
+        case from ~ to ~ dist =>
+          places.addAll(Seq(from, to))
+          paths(from).put(to, dist)
+          paths(to).put(from, dist)
+      }
 
-  def edge: Parser[Unit] = place ~ ("to" ~> place) ~ ("=" ~> distance) ^^ {
-    case from ~ to ~ dist => newPath(from, to, dist)
+      def place: Parser[String] = """\w+""".r
+
+      def distance: Parser[Int] = """\d+""".r ^^ (_.toInt)
+    }
   }
 
-  private def newPath(to: String, from: String, dist: Int): Unit = {
-    places.add(from)
-    places.add(to)
-    paths.getOrElseUpdate(from, new mutable.HashMap[String, Int]()).put(to, dist)
-    paths.getOrElseUpdate(to, new mutable.HashMap[String, Int]()).put(from, dist)
-  }
-
-  // Faerun to Norrath = 144
-  private def place: Parser[String] = """[a-zA-Z]+""".r
-
-  private def distance: Parser[Int] = """\d+""".r ^^ {
-    _.toInt
-  }
+  def solve(input: Seq[String]): (Int, Int) = new Solver(input).minMax
 }
--- src/test/scala/y2015/Day09Spec.scala
+++ src/test/scala/y2015/Day09Spec.scala
@@ -6,29 +6,9 @@ import util.Loader
 
 class Day09Spec extends AnyFlatSpec with Matchers with Day09 {
 
-  "First test" should "pass" in {
-    Loader(this, "day09.txt").foreach { line =>
-      parseAll(edge, line).get
-    }
+  private lazy val input = Loader(this, "day09.txt").toSeq
 
-    val allPaths = places.toSeq.permutations.map { (path: Seq[String]) =>
-      (path, (for edge <- path.sliding(2) yield {
-        paths(edge.head)(edge.tail.head)
-      }).sum)
-    }
-    allPaths.minBy(_._2)._2 shouldBe 117
-  }
-
-  "Second test" should "pass" in {
-    Loader(this, "day09.txt").foreach { line =>
-      parseAll(edge, line).get
-    }
-
-    val allPaths = places.toSeq.permutations.map { (path: Seq[String]) =>
-      (path, (for edge <- path.sliding(2) yield {
-        paths(edge.head)(edge.tail.head)
-      }).sum)
-    }
-    allPaths.maxBy(_._2)._2 shouldBe 909
+  it should "pass parts 1 & 2" in {
+    solve(input) shouldBe(117, 909)
   }
 }
