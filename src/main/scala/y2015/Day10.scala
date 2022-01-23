405f879b1633d70b4a611958ef0ccb9a3082b0ae
Eric Bowman <ebowman@boboco.ie>
2022-01-23 21:14:06 +0100

    modernize 2015.10

--- src/main/scala/y2015/Day10.scala
+++ src/main/scala/y2015/Day10.scala
@@ -1,41 +1,28 @@
 package y2015
 
+import scala.annotation.tailrec
+
 trait Day10 {
-
-  @scala.annotation.tailrec
-  final def iterate(str: String, count: Int): String = {
+  @tailrec final def solve(str: String, count: Int): String =
     if count == 0 then str
-    else iterate(speaknsay(str), count - 1)
-  }
+    else solve(speaknsay(str), count - 1)
 
-  def speaknsay(s: String): String = {
+  def speaknsay(s: String): String =
     var cur: Char = 0
     var count: Int = 0
+    val sb = new StringBuilder
 
-    @scala.annotation.tailrec
-    def recurse(s: String, cursor: Int, b: StringBuilder): StringBuilder = {
-      if cursor == s.length then {
-        if count > 0 then {
-          b.append(count)
-          b.append(cur)
-        }
-        b
-      }
-      else {
-        if s(cursor) == cur then {
-          count += 1
-        } else {
-          if count > 0 then {
-            b.append(count)
-            b.append(cur)
-          }
+    @tailrec def recurse(s: String, cursor: Int): String =
+      if cursor == s.length then
+        if count > 0 then sb.append(count).append(cur)
+        sb.toString
+      else
+        if s(cursor) == cur then count += 1
+        else
+          if count > 0 then sb.append(count).append(cur)
           cur = s(cursor)
           count = 1
-        }
-        recurse(s, cursor + 1, b)
-      }
-    }
+        recurse(s, cursor + 1)
 
-    recurse(s, 0, new StringBuilder).toString
-  }
+    recurse(s, 0)
 }
