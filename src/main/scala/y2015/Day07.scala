b4c12cd4952cf3da8db9927d6189ecb234221283
Eric Bowman <ebowman@boboco.ie>
2022-01-23 19:10:31 +0100

    streamline

--- src/main/scala/y2015/Day07.scala
+++ src/main/scala/y2015/Day07.scala
@@ -11,8 +11,6 @@ trait Day07 {
   }
 
   sealed trait Signal {
-    def name: String
-
     def voltage: Int
   }
 
@@ -24,28 +22,28 @@ trait Day07 {
 
     def parser: Parser[Unit] = wire | wire1 | and | and1 | or | lshift | rshift | _not
 
-    def wire: Parser[Unit] = (num <~ "->") ~ name ^^ { case num ~ name => wires.put(name, Wire(name, num)) }
+    def wire: Parser[Unit] = (num <~ "->") ~ name ^^ { case num ~ name => wires.put(name, Wire(num)) }
 
     def wire1: Parser[Unit] = (name <~ "->") ~ name ^^ { case s ~ name => wires.put(name, Named(s)) }
 
     def and: Parser[Unit] = (name <~ "AND") ~ name ~ ("->" ~> name) ^^ {
-      case s1 ~ s2 ~ d => wires.put(d, OpAnd(d, Named(s1), Named(s2)))
+      case s1 ~ s2 ~ d => wires.put(d, OpAnd(Named(s1), Named(s2)))
     }
 
     def and1: Parser[Unit] = (num <~ "AND") ~ name ~ ("->" ~> name) ^^ {
-      case n ~ s ~ d => wires.put(d, OpAnd(d, Wire("<unnamed>", n), Named(s)))
+      case n ~ s ~ d => wires.put(d, OpAnd(Wire(n), Named(s)))
     }
 
     def or: Parser[Unit] = (name <~ "OR") ~ name ~ ("->" ~> name) ^^ {
-      case s1 ~ s2 ~ d => wires.put(d, OpOr(d, Named(s1), Named(s2)))
+      case s1 ~ s2 ~ d => wires.put(d, OpOr(Named(s1), Named(s2)))
     }
 
     def lshift: Parser[Unit] = name ~ ("LSHIFT" ~> num) ~ ("->" ~> name) ^^ {
-      case s ~ shift ~ d => wires.put(d, OpLShift(d, Named(s), shift))
+      case s ~ shift ~ d => wires.put(d, OpLShift(Named(s), shift))
     }
 
     def rshift: Parser[Unit] = name ~ ("RSHIFT" ~> num) ~ ("->" ~> name) ^^ {
-      case s ~ shift ~ d => wires.put(d, OpRShift(d, Named(s), shift))
+      case s ~ shift ~ d => wires.put(d, OpRShift(Named(s), shift))
     }
 
     def num: Parser[Int] = """\d+""".r ^^ (_.toInt)
@@ -53,31 +51,31 @@ trait Day07 {
     def name: Parser[String] = """[a-z]+""".r
 
     def _not: Parser[Unit] = ("NOT" ~> name) ~ ("->" ~> name) ^^ {
-      case s ~ d => wires.put(d, OpNot(d, Named(s))) }
+      case s ~ d => wires.put(d, OpNot(Named(s))) }
 
     case class Named(name: String) extends Signal {
       lazy val voltage: Int = wires(name).voltage
     }
 
-    case class Wire(name: String, override val voltage: Int) extends Signal
+    case class Wire(override val voltage: Int) extends Signal
 
-    case class OpAnd(name: String, s1: Signal, s2: Signal) extends Signal {
+    case class OpAnd(s1: Signal, s2: Signal) extends Signal {
       override lazy val voltage: Int = s1.voltage & s2.voltage
     }
 
-    case class OpOr(name: String, s1: Signal, s2: Signal) extends Signal {
+    case class OpOr(s1: Signal, s2: Signal) extends Signal {
       override lazy val voltage: Int = s1.voltage | s2.voltage
     }
 
-    case class OpLShift(name: String, s: Signal, shift: Int) extends Signal {
+    case class OpLShift(s: Signal, shift: Int) extends Signal {
       override lazy val voltage: Int = (s.voltage << shift) & 0xFFFF
     }
 
-    case class OpRShift(name: String, s: Signal, shift: Int) extends Signal {
+    case class OpRShift(s: Signal, shift: Int) extends Signal {
       override lazy val voltage: Int = s.voltage >> shift
     }
 
-    case class OpNot(name: String, s: Signal) extends Signal {
+    case class OpNot(s: Signal) extends Signal {
       override lazy val voltage: Int = (~s.voltage) & 0xFFFF
     }
   }
