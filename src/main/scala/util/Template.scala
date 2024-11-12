56a73b3769e30c8df4f041bc6a4e7c2e9da5c4e4
Eric Bowman <ebowman@boboco.ie>
2024-11-12 21:04:42 +0100

    Populates a skeleton for a particular year and day, via e.g. "run 2023 01"

--- src/main/scala/util/Template.scala
+++ src/main/scala/util/Template.scala
@@ -1,9 +1,13 @@
 package util
 
+import java.io.{BufferedReader, PrintWriter}
+import java.nio.file.{Files, Paths}
+
 def mainSrc(year: String, day: String): String =
   s"""
-     |package ${year}
+     |package y${year}
      |
+     |// see https://adventofcode.com/$year/day/${day.toInt}
      |trait Day${day} {
      |
      |}
@@ -11,7 +15,7 @@ def mainSrc(year: String, day: String): String =
 
 def mainTest(year: String, day: String): String =
   s"""
-     |package ${year}
+     |package y${year}
      |
      |import org.scalatest.flatspec.AnyFlatSpec
      |import org.scalatest.matchers.should._
@@ -20,7 +24,9 @@ def mainTest(year: String, day: String): String =
      |class Day${day}Spec extends AnyFlatSpec with Matchers with Day${day} {
      |
      |  lazy val input: IndexedSeq[String] = Loader(this, "day${day}.txt").toIndexedSeq
-     |
+     |  lazy val testInput: IndexedSeq[String] = Loader(this, "day${day}.test.txt").toIndexedSeq
+     |  lazy val testInput2: IndexedSeq[String] = Loader(this, "day${day}.test2.txt").toIndexedSeq
+     |     |
      |  it should "solve part 1 test" in {
      |
      |  }
@@ -39,13 +45,43 @@ def mainTest(year: String, day: String): String =
      |}
      |""".stripMargin
 
+
+def downloadInput(year: String, day: String, dest: String): Unit =
+  import scala.sys.process.*
+  val url = s"https://adventofcode.com/$year/day/${day.toInt}/input"
+  val cookies = "cat .session".!!.trim
+  val conn = new java.net.URI(url).toURL.openConnection().asInstanceOf[java.net.HttpURLConnection]
+  conn.setRequestMethod("GET")
+  conn.setRequestProperty("Cookie", s"session=$cookies")
+  val in = new BufferedReader(new java.io.InputStreamReader(conn.getInputStream))
+  val out = new PrintWriter(dest)
+  var input = in.readLine()
+  while input != null do
+    out.println(input)
+    input = in.readLine()
+  in.close()
+  out.close()
+
 @main def template(args: String*): Unit =
-  val year = s"y${args(0)}"
+  val year = args(0)
   val day = args(1)
-  val srcPath = s"src/main/scala/$year/Day$day.scala"
-  val testPath = s"src/test/scala/$year/Day${day}Spec.scala"
-  val rezPath = s"src/main/resources/$year/day$day.txt"
-  val mainSrcContent = mainSrc(year, args(1))
-  new java.io.PrintWriter(srcPath) { write(mainSrcContent); close() }
-  new java.io.PrintWriter(testPath) { write(mainTest(year, args(1))); close() }
-  new java.io.PrintWriter(rezPath) { write(""); close() }
+  val srcPath = s"src/main/scala/y$year/Day$day.scala"
+  val testPath = s"src/test/scala/y$year/Day${day}Spec.scala"
+  val rezPath = s"src/main/resources/y$year/day$day.txt"
+  val testRezPath = s"src/main/resources/y$year/day$day.test.txt"
+  val testRezPath2 = s"src/main/resources/y$year/day$day.test2.txt"
+  val mainSrcContent = mainSrc(year, day)
+  if !Files.exists(Paths.get(srcPath)) then new PrintWriter(srcPath) {
+    write(mainSrcContent); close()
+  }
+  if !Files.exists(Paths.get(testPath)) then new PrintWriter(testPath) {
+    write(mainTest(year, day)); close()
+  }
+  if !Files.exists(Paths.get(rezPath)) then downloadInput(year, day, rezPath)
+  if !Files.exists(Paths.get(testRezPath)) then new PrintWriter(testRezPath) {
+    write(""); close()
+  }
+  if !Files.exists(Paths.get(testRezPath2)) then new PrintWriter(testRezPath2) {
+    write("");
+    close()
+  }
