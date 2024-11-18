bc48ea1d0d9f657320b9e28215c941924fa81682
Eric Bowman <ebowman@boboco.ie>
2024-11-18 22:46:19 +0100

    Updated to more modern scala

--- src/main/scala/util/Template.scala
+++ src/main/scala/util/Template.scala
@@ -7,9 +7,10 @@ def mainSrc(year: String, day: String): String =
   s"""package y${year}
      |
      |// see https://adventofcode.com/$year/day/${day.toInt}
-     |trait Day${day} {
-     |
-     |}
+     |trait Day${day}:
+     |  def solvePart1(input: Seq[String]): Int = 0
+     |  def solvePart2(input: Seq[String]): Int = 0
+     |end Day${day}
      |""".stripMargin
 
 def mainTest(year: String, day: String): String =
@@ -19,28 +20,23 @@ def mainTest(year: String, day: String): String =
      |import org.scalatest.matchers.should._
      |import util.Loader
      |
-     |class Day${day}Spec extends AnyFlatSpec with Matchers with Day${day} {
+     |class Day${day}Spec extends AnyFlatSpec with Matchers with Day${day}:
      |
      |  lazy val input: IndexedSeq[String] = Loader(this, "day${day}.txt").toIndexedSeq
      |  lazy val testInput: IndexedSeq[String] = Loader(this, "day${day}.test.txt").toIndexedSeq
      |  lazy val testInput2: IndexedSeq[String] = Loader(this, "day${day}.test2.txt").toIndexedSeq
      |
-     |  it should "solve part 1 test" in {
+     |  it should "solve part 1 test" in:
+     |    solvePart1(testInput) shouldBe 0
      |
-     |  }
+     |  it should "solve part 1" in:
+     |    solvePart1(input) shouldBe 0
      |
-     |  it should "solve part 1" in {
+     |  it should "solve part 2 test" in:
+     |    solvePart2(testInput) shouldBe 0
      |
-     |  }
-     |
-     |  it should "solve part 2 test" in {
-     |
-     |  }
-     |
-     |  it should "solve part 2" in {
-     |
-     |  }
-     |}
+     |  it should "solve part 2" in:
+     |    solvePart2(input) shouldBe 0
      |""".stripMargin
 
 
