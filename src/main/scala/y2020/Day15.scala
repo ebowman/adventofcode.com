c0ce83f6cae7f4d5c1be7c63fe912180dcf9dc8a
Eric Bowman <ebowman@boboco.ie>
2024-11-18 21:59:58 +0100

    optimize 2020/15

--- src/main/scala/y2020/Day15.scala
+++ src/main/scala/y2020/Day15.scala
@@ -1,23 +1,22 @@
 package y2020
 
-import collection.mutable
+trait Day15:
+  def solve(input: String, limit: Int): Int =
+    val seeds = input.split(",").map(_.toInt)
+    val memory = new Array[Int](limit)
 
-trait Day15 {
+    var turn = 1
+    seeds.init.foreach: num =>
+      memory(num) = turn
+      turn += 1
 
-  def solve(input: String, limit: Int): Int = {
-    val memory = mutable.Map[Int, (Int, Int)]().withDefault(_ => (-1, -1))
+    var lastNumber = seeds.last
+    while (turn < limit) do
+      val prevTurn = memory(lastNumber)
+      memory(lastNumber) = turn
+      lastNumber = if (prevTurn == 0) 0 else turn - prevTurn
+      turn += 1
 
-    val seeds = input.split(",").map(_.toInt).toIndexedSeq
-    for t <- 1 to seeds.length do memory(seeds(t - 1)) = (t, t)
-    var lastSeen = seeds.last
-    (seeds.length + 1 to limit).foreach(t => {
-        memory(lastSeen) match {
-          case (t0, t1) if t0 == t1 => lastSeen = 0
-          case (_, _) => lastSeen = memory(lastSeen)._2 - memory(lastSeen)._1
-        }
-        if memory(lastSeen) == (-1, -1) then memory(lastSeen) = (t, t)
-        else memory(lastSeen) = (memory(lastSeen)._2, t)
-      })
-    lastSeen
-  }
-}
+    lastNumber
+  end solve
+end Day15
--- src/test/scala/y2020/Day15Spec.scala
+++ src/test/scala/y2020/Day15Spec.scala
@@ -4,11 +4,11 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should._
 import util.Loader
 
-class Day15Spec extends AnyFlatSpec with Matchers with Day15 {
+class Day15Spec extends AnyFlatSpec with Matchers with Day15:
 
   lazy val input: IndexedSeq[String] = Loader(this, "day15.txt").toIndexedSeq
 
-  it should "solve part 1 test" in {
+  it should "solve part 1 test" in:
     solve("0,3,6",2020 ) shouldBe 436
     solve("1,3,2", 2020 ) shouldBe 1
     solve("2,1,3", 2020 ) shouldBe 10
@@ -16,13 +16,11 @@ class Day15Spec extends AnyFlatSpec with Matchers with Day15 {
     solve("2,3,1", 2020) shouldBe 78
     solve("3,2,1", 2020) shouldBe 438
     solve("3,1,2", 2020) shouldBe 1836
-  }
 
-  it should "solve part 1" in {
+  it should "solve part 1" in:
     solve(Loader(this, "day15.txt").mkString, 2020) shouldBe 1522
-  }
 
-  it should "solve part 2 test" in {
+  it should "solve part 2 test" in:
     solve("0,3,6",30000000 ) shouldBe 175594
     solve("1,3,2", 30000000 ) shouldBe 2578
     solve("2,1,3", 30000000 ) shouldBe 3544142
@@ -30,9 +28,6 @@ class Day15Spec extends AnyFlatSpec with Matchers with Day15 {
     solve("2,3,1", 30000000) shouldBe 6895259
     solve("3,2,1", 30000000) shouldBe 18
     solve("3,1,2", 30000000) shouldBe 362
-  }
 
-  it should "solve part 2" in {
+  it should "solve part 2" in:
     solve(Loader(this, "day15.txt").mkString, 30000000) shouldBe 18234
-  }
-}
