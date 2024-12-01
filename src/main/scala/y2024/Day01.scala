e25295a7b031c29e484b5e20f945f048a23c56dd
Eric Bowman <eric.bowman@king.com>
2024-12-01 09:44:28 +0000

    Reworked 2024/01 using new scaffolding

--- src/main/scala/y2024/Day01.scala
+++ src/main/scala/y2024/Day01.scala
@@ -1,17 +1,17 @@
 package y2024
 
-trait Day01:
+class Day01 extends util.Day(1):
   private def parseLine(line: String): (Int, Int) =
     line.trim.split("\\s+") match
       case Array(left, right) => (left.toInt, right.toInt)
       case _ => throw IllegalArgumentException(s"Invalid input line: $line")
 
-  def solvePart1(input: Seq[String]): Int =
+  def solvePart1(input: IndexedSeq[String]): Any =
     val pairs = input.map(parseLine)
     val (left, right) = (pairs.map(_._1).sorted, pairs.map(_._2).sorted)
     left.zip(right).map((l, r) => math.abs(l - r)).sum
 
-  def solvePart2(input: Seq[String]): Int =
+  def solvePart2(input: IndexedSeq[String]): Any =
     val pairs = input.map(parseLine)
     val (left, right) = (pairs.map(_._1), pairs.map(_._2))
     val rightCounts = right.groupBy(identity).view.mapValues(_.size).toMap
--- src/test/scala/y2024/Day01Spec.scala
+++ src/test/scala/y2024/Day01Spec.scala
@@ -1,22 +1,15 @@
 package y2024
 
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should._
-import util.Loader
-
-class Day01Spec extends AnyFlatSpec with Matchers with Day01:
-
-  lazy val input: IndexedSeq[String] = Loader(this, "day01.txt").toIndexedSeq
-  lazy val testInput: IndexedSeq[String] = Loader(this, "day01.test.txt").toIndexedSeq
+class Day01Spec extends util.DaySpec(new Day01):
 
   it should "solve part 1 test" in:
-    solvePart1(testInput) shouldBe 11
+    solution.solvePart1(testInput) shouldBe 11
 
   it should "solve part 1" in:
-    solvePart1(input) shouldBe 1941353
+    solution.solvePart1(input) shouldBe 1941353
 
   it should "solve part 2 test" in:
-    solvePart2(testInput) shouldBe 31
+    solution.solvePart2(testInput) shouldBe 31
 
   it should "solve part 2" in:
-    solvePart2(input) shouldBe 22539317
+    solution.solvePart2(input) shouldBe 22539317
