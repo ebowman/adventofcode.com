1674b3c64761937c885651a6a90d16bfe6188ad6
Eric Bowman <eric.bowman@king.com>
2024-12-03 10:44:08 +0100

    Turd polishing

--- src/main/scala/y2024/Day03.scala
+++ src/main/scala/y2024/Day03.scala
@@ -13,8 +13,7 @@ class Day03 extends util.Day(3):
   def solvePart2(input: IndexedSeq[String]): Int =
     val pattern = """(mul\((\d+),(\d+)\)|do\(\)|don't\(\))""".r
     case class Payload(total: Int = 0, enabled: Boolean = true)
-    input.foldLeft(Payload()):
-      case (payload, line) =>
+    input.foldLeft(Payload()): (payload, line) =>
         pattern.findAllMatchIn(line).foldLeft(payload): (payload, `match`) =>
             `match`.matched match
               case "do()" => payload.copy(enabled = true)
