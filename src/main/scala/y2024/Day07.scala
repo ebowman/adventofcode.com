8063cad773ae09b6d4f7a58238b3021fdbd8ef18
Eric Bowman <ebowman@boboco.ie>
2024-12-07 14:08:06 +0100

    2024/07 perf optimizations (4x speedup)

--- src/main/scala/y2024/Day07.scala
+++ src/main/scala/y2024/Day07.scala
@@ -1,5 +1,7 @@
 package y2024
 
+import scala.annotation.tailrec
+
 class Day07 extends util.Day(7):
   enum Operator:
     case Add, Multiply, Concat
@@ -7,7 +9,26 @@ class Day07 extends util.Day(7):
     def apply(a: Long, b: Long): Long = this match
       case Add => a + b
       case Multiply => a * b
-      case Concat => s"$a$b".toLong
+      case Concat =>
+        def countDigits(n: Long): Int =
+          if n == 0 then 1
+          else
+            @tailrec def recurse(x: Long, count: Int = 0): Int =
+              if (x == 0) count else recurse(x / 10, count + 1)
+
+            recurse(n)
+        end countDigits
+
+        def pow10(exp: Int): Long =
+          @tailrec def recurse(e: Int, acc: Long = 1L): Long =
+            if e == 0 then acc else recurse(e - 1, acc * 10)
+
+          recurse(exp)
+        end pow10
+
+        val digits = countDigits(b)
+        a * pow10(digits) + b
+    end apply
   end Operator
 
   case class Equation(target: Long, numbers: List[Long])
@@ -30,7 +51,7 @@ class Day07 extends util.Day(7):
       yield op :: ops
 
   private def evaluate(numbers: List[Long], operators: List[Operator]): Long =
-    operators.zip(numbers.tail).foldLeft(numbers.head):
+    operators.iterator.zip(numbers.tail).foldLeft(numbers.head):
       case (acc, (op, num)) =>
          op(acc, num)
 
