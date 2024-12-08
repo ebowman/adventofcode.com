fe46c6410e60f22e6ddb22c9da21610129f7461a
Eric Bowman <eric.bowman@king.com>
2024-12-08 07:07:50 +0100

    2024/08 simplified

--- src/main/scala/y2024/Day08.scala
+++ src/main/scala/y2024/Day08.scala
@@ -4,86 +4,70 @@ import scala.annotation.tailrec
 
 class Day08 extends util.Day(8):
 
-  case class Pt(row: Int, col: Int)
-
-  case class Vec(dr: Int, dc: Int)
+  case class Pt(row: Int, col: Int):
+    def isInBounds(height: Int, width: Int): Boolean =
+      row >= 0 && row < height && col >= 0 && col < width
 
   private def parseInput(input: IndexedSeq[String]) =
     val height = input.length
     val width = input.head.length
-    val acc = Map.empty[Char, List[(Int, Int)]].withDefault(_ => Nil)
+    val acc = Map.empty[Char, List[Pt]].withDefaultValue(Nil)
     val antennaGroups = input.zipWithIndex.foldLeft(acc):
       case (acc, (line, row)) =>
         line.zipWithIndex.foldLeft(acc):
           case (map, (char, col)) if char != '.' =>
-            map.updated(char, (row, col) :: map(char))
+            map.updated(char, Pt(row, col) :: map(char))
           case (map, _) => map
     (height, width, antennaGroups)
-  end parseInput
 
   def solvePart1(input: IndexedSeq[String]): Int =
     val (height, width, antennaGroups) = parseInput(input)
+    val antinodesBuilder = Set.newBuilder[Pt]
 
-    val antinodes = for
+    for
       positions <- antennaGroups.values
-      (p1, i) <- positions.zipWithIndex
-      p2 <- positions.drop(i + 1)
-      antinode <- calculateDoubleDistanceAntinodes(Pt(p1._1, p1._2), Pt(p2._1, p2._2))
-      if isInBounds(antinode, height, width)
-    yield antinode
+      i <- positions.indices
+      p1 = positions(i)
+      j <- 0 until i
+      p2 = positions(j)
+      dr = p2.row - p1.row
+      dc = p2.col - p1.col
+      forward = Pt(p2.row + dr, p2.col + dc)
+      reverse = Pt(p1.row - dr, p1.col - dc)
+    do
+      if forward.isInBounds(height, width) then antinodesBuilder += forward
+      if reverse.isInBounds(height, width) then antinodesBuilder += reverse
 
-    antinodes.toSet.size
-  end solvePart1
-
-  private def calculateDoubleDistanceAntinodes(p1: Pt, p2: Pt): Set[Pt] =
-    val diff = Vec(p2.row - p1.row, p2.col - p1.col)
-    Set(
-      Pt(p1.row - diff.dr, p1.col - diff.dc),
-      Pt(p2.row + diff.dr, p2.col + diff.dc)
-    )
-
-  private def isInBounds(p: Pt, height: Int, width: Int): Boolean =
-    p.row >= 0 && p.row < height && p.col >= 0 && p.col < width
+    val antinodes = antinodesBuilder.result()
+    antinodes.size
 
   def solvePart2(input: IndexedSeq[String]): Int =
     val (height, width, antennaGroups) = parseInput(input)
+    val antinodesBuilder = Set.newBuilder[Pt]
 
-    val allAntinodes = for
+    def addLineAntinodes(p1: Pt, p2: Pt): Unit =
+      val dr = p2.row - p1.row
+      val dc = p2.col - p1.col
+
+      @tailrec
+      def recurse(pt: Pt): Unit =
+        if pt.isInBounds(height, width) then
+          antinodesBuilder += pt
+          recurse(Pt(pt.row + dr, pt.col + dc))
+
+      recurse(p2)
+    end addLineAntinodes
+
+    for
       positions <- antennaGroups.values
-      if positions.size >= 2
-      points = positions.map(p => Pt(p._1, p._2))
-      lines = findAllLines(points)
-      point <- generateAntinodesForLine(lines, height, width)
-    yield point
+      i <- positions.indices
+      j <- 0 until i
+      p1 = positions(i)
+      p2 = positions(j)
+    do
+      addLineAntinodes(p1, p2)
+      addLineAntinodes(p2, p1)
 
-    allAntinodes.toSet.size
-  end solvePart2
+    val antinodes = antinodesBuilder.result()
+    antinodes.size
 
-  private def findAllLines(points: List[Pt]): Set[(Pt, Vec)] =
-    val pairs = for
-      (p1, i) <- points.zipWithIndex
-      p2 <- points.drop(i + 1)
-    yield (p1, p2)
-
-    pairs.map:
-      case (p1, p2) =>
-        val dr = p2.row - p1.row
-        val dc = p2.col - p1.col
-        val gcdValue = gcd(dr, dc)
-        val direction = Vec(dr / gcdValue, dc / gcdValue)
-        (p1, direction)
-    .toSet
-  end findAllLines
-
-  private def generateAntinodesForLine(lines: Set[(Pt, Vec)], height: Int, width: Int): Set[Pt] =
-    lines.flatMap:
-      case (start, dir) =>
-        for
-          t <- -math.max(height, width) to math.max(height, width)
-          point = Pt(start.row + t * dir.dr, start.col + t * dir.dc)
-          if isInBounds(point, height, width)
-        yield point
-  end generateAntinodesForLine
-
-  @tailrec private def gcd(a: Int, b: Int): Int =
-    if b == 0 then math.abs(a) else gcd(b, a % b)
