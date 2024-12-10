7e75fbe973970e2c5794b52583ffa01b18a8a79f
Eric Bowman <eric.bowman@king.com>
2024-12-10 08:52:20 +0100

    2024/10 - much more elegant

--- src/main/scala/y2024/Day10.scala
+++ src/main/scala/y2024/Day10.scala
@@ -1,76 +1,38 @@
 package y2024
 
-import scala.annotation.tailrec
-import scala.util.Try
-
 class Day10 extends util.Day(10):
 
   def solvePart1(input: IndexedSeq[String]): Int =
-    val grid = input.map(_.map(_.asDigit).toVector).toVector
-    val reachableNines = buildReachableNinesTable(grid)
-    (for
-      r <- grid.indices
-      c <- grid(r).indices
-      if grid(r)(c) == 0
-    yield reachableNines(r)(c).size).sum
+    val (area, trailheads) = parseInput(input)
+    trailheads.map: start =>
+      findTrails(start, area).map(_.last).toSet.size
+    .sum
 
   def solvePart2(input: IndexedSeq[String]): Int =
-    val grid = input.map(_.map(_.asDigit).toVector).toVector
-    val trailCounts = buildTrailCountsTable(grid)
-    (for
-      r <- grid.indices
-      c <- grid(r).indices
-      if grid(r)(c) == 0
-    yield trailCounts(r)(c)).sum
+    val (area, trailheads) = parseInput(input)
+    trailheads.map: start =>
+      findTrails(start, area).size
+    .sum
 
-  private def buildTrailCountsTable(grid: Vector[Vector[Int]]): Vector[Vector[Int]] =
-    val (rows, cols) = (grid.length, grid(0).length)
-    val allCells = for r <- 0 until rows; c <- 0 until cols yield (r, c)
-    val reachableNines = buildReachableNinesTable(grid)
+  private def parseInput(input: IndexedSeq[String]): (Map[(Int, Int), Int], Seq[(Int, Int)]) =
+    val area = (for
+      y <- input.indices
+      x <- input.head.indices
+    yield (x, y) -> input(y)(x).asDigit).toMap
+    (area, area.keys.filter(xy => area(xy) == 0).toSeq)
 
-    @tailrec def recurse(h: Int, acc: Vector[Vector[Int]]): Vector[Vector[Int]] =
-      if h < 0 then acc
+  private def findTrails(start: (Int, Int), area: Map[(Int, Int), Int]): List[List[(Int, Int)]] =
+    def recurse(xy: (Int, Int)): List[List[(Int, Int)]] =
+      val currentHeight = area(xy)
+      if currentHeight == 9 then List(List(xy))
       else
-        val cellsAtHeight = allCells.filter(rc => grid(rc._1)(rc._2) == h)
-        val updatedAcc = cellsAtHeight.foldLeft(acc):
-          case (innerAcc, (r, c)) =>
-            if h == 9 then innerAcc.updated(r, innerAcc(r).updated(c, 1))
-            else
-              val neighbors = Seq((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)).filter:
-                case (nr, nc) =>
-                  nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid(nr)(nc) == h + 1 && reachableNines(nr)(nc).nonEmpty
-              val sumCount = neighbors.foldLeft(0):
-                case (acc2, (nr, nc)) =>
-                  acc2 + innerAcc(nr)(nc)
-              innerAcc.updated(r, innerAcc(r).updated(c, sumCount))
-        recurse(h - 1, updatedAcc)
+        val nextHeight = currentHeight + 1
+        for
+          direction <- List((-1, 0), (1, 0), (0, -1), (0, 1))
+          nextCell = (xy._1 + direction._1, xy._2 + direction._2)
+          if area.get(nextCell).contains(nextHeight)
+          trail <- recurse(nextCell)
+        yield xy :: trail
 
-    val initialTrailCounts = Vector.fill(rows, cols)(0)
-    recurse(9, initialTrailCounts)
-
-  private def buildReachableNinesTable(grid: Vector[Vector[Int]]): Vector[Vector[Set[(Int, Int)]]] =
-    val (rows, cols) = (grid.length, grid(0).length)
-    val allCells = for r <- 0 until rows; c <- 0 until cols yield (r, c)
-
-    @tailrec def recurse(h: Int, acc: Vector[Vector[Set[(Int, Int)]]]): Vector[Vector[Set[(Int, Int)]]] =
-      if h < 0 then acc
-      else
-        val cellsAtHeight = allCells.filter:
-          case (r, c) =>
-            grid(r)(c) == h
-        val updatedAcc = cellsAtHeight.foldLeft(acc):
-          case (innerAcc, rc@(r, c)) =>
-            if h == 9 then innerAcc.updated(r, innerAcc(r).updated(c, Set(rc)))
-            else
-              val neighbors = Seq((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)).filter:
-                case (nr, nc) =>
-                  Try(grid(nr)(nc) == h + 1).getOrElse(false)
-              val unionSet = neighbors.foldLeft(Set.empty[(Int, Int)]):
-                case (acc2, (nr, nc)) =>
-                  acc2.union(innerAcc(nr)(nc))
-              innerAcc.updated(r, innerAcc(r).updated(c, unionSet))
-        recurse(h - 1, updatedAcc)
-
-    val initialReachableNines = Vector.fill(rows, cols)(Set.empty[(Int, Int)])
-    recurse(9, initialReachableNines)
+    recurse(start)
 end Day10
