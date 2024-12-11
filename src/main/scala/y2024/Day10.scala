07f1b0210681d25e4eda4dc228428e7a9a13d563
Eric Bowman <eric.bowman@king.com>
2024-12-11 05:57:42 +0100

    2024/10 - turd polishing

--- src/main/scala/y2024/Day10.scala
+++ src/main/scala/y2024/Day10.scala
@@ -2,10 +2,14 @@ package y2024
 
 class Day10 extends util.Day(10):
 
+  private case class Coord(x: Int, y: Int)
+
+  private val Directions = List(Coord(-1, 0), Coord(1, 0), Coord(0, -1), Coord(0, 1))
+
   def solvePart1(input: IndexedSeq[String]): Int =
     val (area, trailheads) = parseInput(input)
     trailheads.map: start =>
-      findTrails(start, area).map(_.last).toSet.size
+      findTrails(start, area).map(_.last).distinct.size
     .sum
 
   def solvePart2(input: IndexedSeq[String]): Int =
@@ -14,25 +18,35 @@ class Day10 extends util.Day(10):
       findTrails(start, area).size
     .sum
 
-  private def parseInput(input: IndexedSeq[String]): (Map[(Int, Int), Int], Seq[(Int, Int)]) =
+  private def parseInput(input: IndexedSeq[String]): (Map[Coord, Int], Seq[Coord]) =
+
     val area = (for
       y <- input.indices
       x <- input.head.indices
-    yield (x, y) -> input(y)(x).asDigit).toMap
-    (area, area.keys.filter(xy => area(xy) == 0).toSeq)
+    yield Coord(x, y) -> input(y)(x).asDigit).toMap
 
-  private def findTrails(start: (Int, Int), area: Map[(Int, Int), Int]): List[List[(Int, Int)]] =
-    def recurse(xy: (Int, Int)): List[List[(Int, Int)]] =
-      val currentHeight = area(xy)
-      if currentHeight == 9 then List(List(xy))
+    val trailheads = area.collect:
+      case (coord, 0) => coord
+    .toSeq
+
+    (area, trailheads)
+  end parseInput
+
+  private def findTrails(start: Coord, area: Map[Coord, Int]): List[List[Coord]] =
+
+    def recurse(pos: Coord): List[List[Coord]] =
+      val currentHeight = area(pos)
+      if currentHeight == 9 then List(List(pos))
       else
         val nextHeight = currentHeight + 1
         for
-          direction <- List((-1, 0), (1, 0), (0, -1), (0, 1))
-          nextCell = (xy._1 + direction._1, xy._2 + direction._2)
+          direction <- Directions
+          nextCell = Coord(x = pos.x + direction.x, y = pos.y + direction.y)
           if area.get(nextCell).contains(nextHeight)
           trail <- recurse(nextCell)
-        yield xy :: trail
+        yield pos :: trail
 
     recurse(start)
+  end findTrails
+
 end Day10
