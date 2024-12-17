2a3223197f00c94124596c3848a464711cbae8bc
Eric Bowman <ebowman@boboco.ie>
2024-12-17 17:21:36 +0100

    2024/17 turd polishing

--- src/main/scala/y2024/Day17.scala
+++ src/main/scala/y2024/Day17.scala
@@ -6,12 +6,31 @@ import scala.annotation.tailrec
 class Day17 extends util.Day(17):
 
   private type Registers = Map[Char, Long]
+  private type Instruction = (Int, Registers) => (Int, Option[Long], Registers)
   private val instructions: Vector[Instruction] = Vector(
-    ShiftRegister('A'), // Adv
-    Bxl, Bst, Jnz, Bxc, Out,
-    ShiftRegister('B'), // Bdv
-    ShiftRegister('C') // Cdv
+    /* adv */ shiftRegister('A'),
+    /* bxl */ (param, regs) =>
+      val updated = regs + ('B' -> (regs('B') ^ param))
+      (-1, None, updated),
+    /* bst */ (param, regs) =>
+      val updated = regs + ('B' -> (regs.lookup(param) & 7))
+      (-1, None, updated),
+    /* jnz */ (param, regs) =>
+      if regs('A') == 0 then (-1, None, regs) else (param, None, regs),
+    /* bxc */ (_, regs) =>
+      val updated = regs + ('B' -> (regs('B') ^ regs('C')))
+      (-1, None, updated),
+    /* out */ (param, regs) =>
+      val output = regs.lookup(param) & 7
+      (-1, Some(output), regs),
+    /* bdv */ shiftRegister('B'),
+    /* cdv */ shiftRegister('C')
   )
+
+  private def shiftRegister(reg: Char)(param: Int, regs: Registers) =
+    val shift = regs.lookup(param)
+    val updated = regs + (reg -> (regs('A') >> shift))
+    (-1, None, updated)
 
   def solvePart1(input: IndexedSeq[String]): Any =
     val (ra, program) = parseInput(input)
@@ -30,10 +49,9 @@ class Day17 extends util.Day(17):
       if ptr >= program.length then
         outputAcc
       else
-        val inst = program(ptr)
+        val instIndex = program(ptr)
         val param = if ptr + 1 < program.length then program(ptr + 1) else 0
-
-        val (nextPtr, maybeOut, updatedRegs) = instructions(inst).exec(param, regs)
+        val (nextPtr, maybeOut, updatedRegs) = instructions(instIndex)(param, regs)
         val newPtr = if nextPtr >= 0 then nextPtr else ptr + 2
         val newOutput = maybeOut.fold(outputAcc)(outputAcc :+ _)
 
@@ -63,39 +81,6 @@ class Day17 extends util.Day(17):
       .nextOption()
   end findA
 
-  sealed trait Instruction:
-    def exec(i: Int, registers: Registers): (Int, Option[Long], Registers)
-
-  private case class ShiftRegister(targetReg: Char) extends Instruction:
-    def exec(i: Int, regs: Registers): (Int, Option[Long], Registers) =
-      val shift = regs.lookup(i)
-      val updated = regs + (targetReg -> (regs('A') >> shift))
-      (-1, None, updated)
-
-  private case object Bxl extends Instruction:
-    def exec(i: Int, regs: Registers): (Int, Option[Long], Registers) =
-      val updated = regs + ('B' -> (regs('B') ^ i))
-      (-1, None, updated)
-
-  private case object Bst extends Instruction:
-    def exec(i: Int, regs: Registers): (Int, Option[Long], Registers) =
-      val updated = regs + ('B' -> (regs.lookup(i) & 7))
-      (-1, None, updated)
-
-  private case object Jnz extends Instruction:
-    def exec(i: Int, regs: Registers): (Int, Option[Long], Registers) =
-      if regs('A') == 0 then (-1, None, regs) else (i, None, regs)
-
-  private case object Bxc extends Instruction:
-    def exec(i: Int, regs: Registers): (Int, Option[Long], Registers) =
-      val updated = regs + ('B' -> (regs('B') ^ regs('C')))
-      (-1, None, updated)
-
-  private case object Out extends Instruction:
-    def exec(i: Int, regs: Registers): (Int, Option[Long], Registers) =
-      val output = regs.lookup(i) & 7
-      (-1, Some(output), regs)
-
   private given registersOps: AnyRef with
     extension (regs: Registers)
       def lookup(i: Int): Long = i match
@@ -103,5 +88,5 @@ class Day17 extends util.Day(17):
         case 5 => regs('B')
         case 6 => regs('C')
         case _ => i
-
+    end extension
 end Day17
